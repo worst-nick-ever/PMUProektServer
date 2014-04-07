921f0a7ee494f349b6aeb396d896bce550190b58
worst-nick-ever <firelord@abv.bg>
2014-04-07 21:34:58 +0200

    first commit
    first

---
+++ PMUProektServer/Controllers/AccountApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using PMUProektServer.Models;
+
+namespace PMUProektServer.Controllers
+{
+    public class AccountApiController : ApiController
+    {
+        KursovProektPMUEntities db = new KursovProektPMUEntities();
+
+        //  api/account/login
+        [HttpPost]
+        [ActionName("Login")]
+        public HttpResponseMessage Login(Account acc)
+        {
+            IEnumerable<Account> accs = db.Account.Where(a => a.Name.Equals(acc.Name)).ToList();
+            if (accs.Count() > 0)
+            {
+                if (accs.First().Password.Equals(acc.Password))
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ PMUProektServer/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net.Http;
+using PMUProektServer.Models;
+
+namespace PMUProektServer.Controllers
+{
+    public class AccountController : Controller
+    {
+        KursovProektPMUEntities db = new KursovProektPMUEntities();
+        //
+        // GET: /Account/Insert
+        [ActionName("Insert")]
+        public ActionResult Insert()
+        {
+            return View("InsertAccount");
+        }
+
+        [HttpPost]
+        [ActionName("InsertAcc")]
+        public JsonResult Insert(Account acc)
+        {
+            db.Account.Add(acc);
+            db.SaveChanges();
+            return Json(acc, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ActionName("login")]
+        public ActionResult Login(Account acc)
+        {
+            IEnumerable<Account> accs = db.Account.Where(a => a.Name.Equals(acc.Name)).ToList();
+            if (accs.Count() > 0)
+            {
+                if (accs.First().Password.Equals(acc.Password))
+                {
+                    return View("LoggedIn");
+                }
+                else
+                {
+                    return View("InsertAccount");
+                }
+            }
+            else
+            {
+                return View("InsertAccount");
+            }
+        }
+    }
+}
