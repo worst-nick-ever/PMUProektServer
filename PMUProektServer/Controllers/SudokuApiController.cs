6667398ec43db039d78e9d609f0139b3b4deb362
worst-nick-ever <firelord@abv.bg>
2014-04-14 17:32:08 +0200

    get sudoku from server

---
+++ PMUProektServer/Controllers/SudokuApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PMUProektServer.Models;
+
+namespace PMUProektServer.Controllers
+{
+    public class SudokuApiController : ApiController
+    {
+        KursovProektPMUEntities db = new KursovProektPMUEntities();
+
+        // proektpmu.apphb.com/api/SudokuApi/GetSudoku/difficulty
+        [HttpGet]
+        [ActionName("GetSudoku")]
+        public HttpResponseMessage GetSudoku([FromUri]string difficulty)
+        {
+            var sudokus = db.Sudoku.Where(s => s.Difficulty.Equals(difficulty)).ToList();
+            Sudoku result = null;
+
+            while (result == null)
+            {
+                Random r = new Random();
+                result = sudokus.ElementAt(r.Next(0, sudokus.Count() - 1));
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+    }
+}
